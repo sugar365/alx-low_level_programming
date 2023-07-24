5fcdeb3399fb7f77e5513bef34aa5a2b9b97a373
Ferdinand <ferdinandk70@gmail.com>
2023-07-24 13:55:43 +0000

    fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -25,22 +25,23 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-																								digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-																							}
-		i++;
+	if (s[i] >= '0' && s[i] <= '9')
+	{
+		digit = s[i] - '0';
+		if (d % 2)
+			digit = -digit;
+		n = n * 10 + digit;
+		f = 1;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+			break;
+		f = 0;
 	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	i++;
 }
+
+if (f == 0)
+	return (0);
+
+return (n);
+}
+
